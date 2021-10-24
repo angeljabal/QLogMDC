542bf8fbdfe7c674d5e9c91d1d23b0749305fdce
Noobgrammer <37568966+fingersandmind@users.noreply.github.com>
2021-10-25 02:32:06 +0800

    Update User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->hasProfile()->create([
+            'name'      => 'Alvin Gwapo',
+            'email'     => 'admin@admin.com',
+            'password'  => Hash::make('password'),
+            'type'      => 2,
+            'role'      => 2, 
+        ]);
         User::factory(50)
             ->hasProfile()
             ->create();
